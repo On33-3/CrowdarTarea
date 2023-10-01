7b42c9b7e73d6dcf880ec62467c40578e880984c
On33-3 <71417194+On33-3@users.noreply.github.com>
2023-10-01 15:12:11 -0300

    Tp3 Steps
    steps 2  y 4

--- src/main/java/lippia/web/constants/HomePageConstants.java
+++ src/main/java/lippia/web/constants/HomePageConstants.java
@@ -2,5 +2,6 @@ package lippia.web.constants;
 
 public class HomePageConstants {
 
-
+    public static final String SHOP_MENU_BUTTON = "//a[text()='Shop']";//locator
+    public static final String IMAGE_ARRIVAL = "//img[@alt='Selenium Ruby']"; //locator
 }
--- src/main/java/lippia/web/services/HomePageService.java
+++ src/main/java/lippia/web/services/HomePageService.java
@@ -1,8 +1,18 @@
 package lippia.web.services;
 
 import com.crowdar.core.actions.ActionManager;
+import lippia.web.constants.HomePageConstants;
+
 
 public class HomePageService extends ActionManager {
 
 
+    public static void clickShopMenuButton(){
+        click(HomePageConstants.SHOP_MENU_BUTTON);
+
+    }
+
+    public static void clicksAnArrivalPicture(){
+        click(HomePageConstants.IMAGE_ARRIVAL);
+    }
 }
